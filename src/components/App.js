cbb3a6a98d3739455e14cce13b20ef1a710b4b80
Manoj Saini <manojsaini513@gmail.com>
2023-08-27 14:39:12 +0000

    done5

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,7 @@ const App = () => {
           todos.map((element,index)=>(
             <li key={index}>
               <div>{element}</div>
-              <button onClick={()=>removeTodo(index)}>Delete</button>
+              <button onClick={()=>removeTodo(index)}></button>
             </li>
           ))
         }
